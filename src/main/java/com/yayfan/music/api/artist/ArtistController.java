c58bdb7a0aed260672ac3c4dca6e1387e628cc7d
yaysfan <yaysfan@gmail.com>
2025-08-24 19:00:21 +0900

    아티스트 관련 dto 추가 및 컨트롤러 추가

---
+++ src/main/java/com/yayfan/music/api/artist/ArtistController.java
@@ -0,0 +1,31 @@
+package com.yayfan.music.api.artist;
+
+import com.yayfan.music.domain.artist.Artist;
+import com.yayfan.music.domain.artist.ArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/artists")
+@RequiredArgsConstructor
+public class ArtistController {
+    private final ArtistService service;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<SearchedArtistDto> searchArtists(@RequestBody Optional<String> search) {
+        List<Artist> artists = service.searchArtists(search.orElse(""));
+        return SearchedArtistDto.from(artists);
+    }
+
+    @Secured("ADMIN")
+    @DeleteMapping("{artistId}")
+    public void deleteArtist(@PathVariable("artistId") Integer id) {
+        service.deleteArtist(id);
+    }
+}
