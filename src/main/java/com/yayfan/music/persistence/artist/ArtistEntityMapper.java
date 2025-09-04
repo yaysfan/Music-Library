7076e74e2245c0920d957832507e7f6b3a07d1e1
yaysfan <yaysfan@gmail.com>
2025-09-04 19:22:26 +0900

    Mapper 의존성 분리

--- src/main/java/com/yayfan/music/persistence/artist/ArtistEntityMapper.java
+++ src/main/java/com/yayfan/music/persistence/artist/ArtistEntityMapper.java
@@ -7,11 +7,14 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 @Named("ArtistEntityMapper")
-@Mapper(componentModel = "spring", uses = SongEntityMapper.class)
+@Mapper(componentModel = "spring")
 public interface ArtistEntityMapper {
     @Mapping(target = "songs", ignore = true)
     ArtistEntity toEntity(Artist artist);
 
-    @Mapping(target = "songs", qualifiedByName = "toDomainWithoutArtist")
+    @Mapping(target = "songs", ignore = true)
     Artist toDomain(ArtistEntity entity);
+
+    @Named("toDomainWithSongs")
+    Artist toDomainWithSongs(ArtistEntity entity);
 }
