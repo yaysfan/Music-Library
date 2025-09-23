4380e10b273d24f87740f2541bab432a422706dd
yaysfan <yaysfan@gmail.com>
2025-09-23 10:42:42 +0900

    playlistService 테스트코드 추가

--- src/test/java/com/yayfan/music/domain/playlist/PlaylistServiceTest.java
+++ src/test/java/com/yayfan/music/domain/playlist/PlaylistServiceTest.java
@@ -2,12 +2,14 @@ package com.yayfan.music.domain.playlist;
 
 import com.yayfan.music.domain.user.User;
 import com.yayfan.music.domain.user.UserStorage;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Collections;
 import java.util.List;
@@ -49,6 +51,7 @@ public class PlaylistServiceTest {
     }
 
     @Test
+    @DisplayName("자신의 플레이리스트 목록 조회 성공")
     void shouldFindMyPlaylists() {
         //given
         when(userStorage.findByUsername("testuser")).thenReturn(Optional.of(testUser));
@@ -58,12 +61,39 @@ public class PlaylistServiceTest {
         List<Playlist> myPlaylists = playlistService.findMyPlaylists("testuser");
 
         //then
-        assertEquals(1,myPlaylists.size());
-        assertEquals("test",myPlaylists.get(0).getName());
-        assertEquals("testuser",myPlaylists.get(0).getUser().getUsername());
+        assertEquals(1, myPlaylists.size());
+        assertEquals("test", myPlaylists.get(0).getName());
+        assertEquals("testuser", myPlaylists.get(0).getUser().getUsername());
 
         verify(userStorage, times(1)).findByUsername("testuser");
         verify(playlistStorage, times(1)).findByUserId(testUser.getId());
     }
 
+    @Test
+    @DisplayName("자신의 플레이리스트 목록 조회 실패")
+    void shouldThrowException_whenUserNotFound() {
+        //given
+        when(userStorage.findByUsername("nonexistentUser")).thenReturn(Optional.empty());
+
+        //when + then
+        assertThrows(UsernameNotFoundException.class, () -> {
+            playlistService.findMyPlaylists("nonexistentUser");
+        });
+
+        verify(playlistStorage, never()).findByUserId(anyInt());
+
+    }
+
+    @Test
+    @DisplayName("자신의 플레이리스트 삭제")
+    void shouldDeleteMyPlaylist() {
+        //given
+        when(playlistStorage.findByIdWithSongs(1)).thenReturn(Optional.of(testPlaylist));
+
+        //when
+        playlistService.deletePlaylist(1,"testuser");
+
+        //then
+        verify(playlistStorage, times(1)).delete(1);
+    }
 }
