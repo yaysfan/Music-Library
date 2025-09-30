2d25c8353d72b49eae085cb957d8b410c3b9dfc4
yaysfan <yaysfan@gmail.com>
2025-09-30 11:47:13 +0900

    test 수정

--- src/test/java/com/yayfan/music/MusicApplicationTests.java
+++ src/test/java/com/yayfan/music/MusicApplicationTests.java
@@ -4,7 +4,9 @@ import com.yayfan.music.integration.conversion.YtDlpAdapter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({"test"})
 @SpringBootTest
 class MusicApplicationTests {
 
