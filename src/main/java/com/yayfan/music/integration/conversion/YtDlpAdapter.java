849e3fc69ba9fa3c882d43055233a441043b2cec
yaysfan <yaysfan@gmail.com>
2025-09-30 12:03:02 +0900

    test 수정

--- src/main/java/com/yayfan/music/integration/conversion/YtDlpAdapter.java
+++ src/main/java/com/yayfan/music/integration/conversion/YtDlpAdapter.java
@@ -2,6 +2,7 @@ package com.yayfan.music.integration.conversion;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import java.nio.file.Paths;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+@Profile("!test")
 @Component
 @Slf4j
 public class YtDlpAdapter {
